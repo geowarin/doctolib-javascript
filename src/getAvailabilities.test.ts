59b9197831c16b3c6382480799eeee38fc5f379c
Geoffroy Warin <code@geowarin.com>
2020-01-11 17:22:45 +0100

    Add empty week test

--- src/getAvailabilities.test.ts
+++ src/getAvailabilities.test.ts
@@ -2,7 +2,20 @@ import knex from '../knexClient'
 import getAvailabilities from './getAvailabilities'
 
 describe('getAvailabilities', () => {
-  beforeEach(() => knex('events').truncate())
+  beforeEach(() => knex('events').truncate());
+
+  describe('empty week', () => {
+
+    it('should fetch availabilities correctly', async () => {
+      const availabilities = await getAvailabilities(new Date('2014-08-10')); // sunday
+
+      expect(availabilities.map(a => a.date))
+        .toEqual(['2014-08-10', '2014-08-11', '2014-08-12', '2014-08-13', '2014-08-14', '2014-08-15', '2014-08-16']);
+
+      expect(availabilities.flatMap(a => a.slots))
+        .toEqual([]);
+    });
+  });
 
   describe('simple case', () => {
     beforeEach(async () => {
@@ -19,32 +32,32 @@ describe('getAvailabilities', () => {
           ends_at: new Date('2014-08-11 11:30'),
         },
       ])
-    })
+    });
 
     it('should fetch availabilities correctly', async () => {
-      const availabilities = await getAvailabilities(new Date('2014-08-10')) // sunday
-      expect(availabilities.length).toBe(7)
+      const availabilities = await getAvailabilities(new Date('2014-08-10')); // sunday
+      expect(availabilities.length).toBe(7);
 
       expect(String(availabilities[0].date)).toBe(
         String(new Date('2014-08-10')),
-      )
-      expect(availabilities[0].slots).toEqual([])
+      );
+      expect(availabilities[0].slots).toEqual([]);
 
       expect(String(availabilities[1].date)).toBe(
         String(new Date('2014-08-11')),
-      )
+      );
       expect(availabilities[1].slots).toEqual([
         '9:30',
         '10:00',
         '11:30',
         '12:00',
-      ])
+      ]);
 
-      expect(availabilities[2].slots).toEqual([])
+      expect(availabilities[2].slots).toEqual([]);
 
       expect(String(availabilities[6].date)).toBe(
         String(new Date('2014-08-16')),
       )
     })
   })
-})
+});
